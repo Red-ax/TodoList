a9273bf64d6e1eec648a9af72d9cd5debf0496d1
redax <deev.go@gmail.com>
2023-09-15 16:10:01 +0700

    Save data in UserDefaults

--- TodoList/ViewModels/ListViewModel.swift
+++ TodoList/ViewModels/ListViewModel.swift
@@ -8,22 +8,39 @@
 import Foundation
 
 class ListViewModel: ObservableObject {
-    @Published var items: [ItemModel] = []
+    
+    @Published var items: [ItemModel] = [] {
+        didSet {
+            saveItems()
+        }
+    }
+    
+    let itemsKey: String = "items_list"
     
     init() {
         getItems()
     }
     
     func getItems() {
-        let newItems = [
-            ItemModel(title: "This is the first title!", isCompleted: false),
-            ItemModel(title: "This is the second title!", isCompleted: true),
-            ItemModel(title: "This is the third title!", isCompleted: true),
-            ItemModel(title: "This is the fourth title!", isCompleted: false),
-            ItemModel(title: "This is the fifth title!", isCompleted: true),
-        ]
+//        let newItems = [
+//            ItemModel(title: "This is the first title!", isCompleted: false),
+//            ItemModel(title: "This is the second title!", isCompleted: true),
+//            ItemModel(title: "This is the third title!", isCompleted: true),
+//            ItemModel(title: "This is the fourth title!", isCompleted: false),
+//            ItemModel(title: "This is the fifth title!", isCompleted: true),
+//        ]
+//        
+//        items.append(contentsOf: newItems)
         
-        items.append(contentsOf: newItems)
+//        guard let data = UserDefaults.standard.data(forKey: itemsKey) else { return }
+//        guard let saveItems = try? JSONDecoder().decode([ItemModel].self, from: data) else { return }
+        
+        guard
+            let data = UserDefaults.standard.data(forKey: itemsKey),
+            let saveItems = try? JSONDecoder().decode([ItemModel].self, from: data)
+        else { return }
+        
+        self.items = saveItems
     }
     
     func deleteItem(indexSet: IndexSet) {
@@ -42,6 +59,12 @@ class ListViewModel: ObservableObject {
     func updateItem(item: ItemModel) {
         if let index = items.firstIndex(where: { $0.id == item.id }) {
             items[index] = item.updateCompletion()
+        }
+    }
+    
+    func saveItems() {
+        if let encodedData = try? JSONEncoder().encode(items) {
+            UserDefaults.standard.set(encodedData, forKey: itemsKey)
         }
     }
     
